2e6b4336b5f1360875ff2e2b58964966a6aa4c2b
Cavacalcante <lucascavalcante17@hotmail.com>
2019-06-03 14:57:32 -0300

    Resolver o desafio 008 do curso de Python
    Curso: Curso em vídeo

---
+++ ex008.py
@@ -0,0 +1,5 @@
+valor = int(input('Digite um valor em metros: '))
+centimetros = valor*100
+milimetros = centimetros*10
+
+print('{} metro(s) tem {} centímetros e {} milimetros'.format(valor, centimetros, milimetros))
